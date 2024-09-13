241082a1cd728c5a0c00ac7dab2b0bee4e9c35f8
ARIANE CRISTINA FRANCISCO <ariane.cfrancisco@senacsp.edu.br>
2024-09-12 22:08:10 -0300

    setup do ambiente de desenvolvimento

---
+++ hello.js
@@ -0,0 +1,27 @@
+/**
+ * hello World
+ * @author Ariane Cristina
+ */
+
+console.log("hello world")
+
+____________________________
+
+Node -  Plataforma que permite a execução do JavaScript fora do navegador (browser)
+https://nodejs.org/ (baixar sempre a versão LTS)
+
+comandos básicos:
+node -v
+
+turbinar o node:
+gerenciador de pacotes para o node 
+npm está relacionado a pasta do projeto que vamos usar, ou seja os pacotes são intalados na raiz 
+do projeto e não so sistema operacional: individual
+
+npm -v (versão do Node)
+npm ls (listar pacotes instalados)
+npm Init(iniciar um novo projeto javaScript)
+
+____ package.json
+
+json (JavaScript Object Notation)
