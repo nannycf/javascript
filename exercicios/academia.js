73e7271c9ecd0823dd842a820d00a1c71904db53
Ariane Cristina <arianecristina.93@hotmail.com>
2024-09-19 22:17:46 -0300

    estudos variaveis

---
+++ exercicios/academia.js
@@ -0,0 +1,30 @@
+/**
+*Ficha de um aluno de academia
+* @author Ariane Cristina
+*/
+
+// variáveis
+    let nome
+    let idade
+    let peso
+    let altura
+    let VIP
+
+
+console.clear()
+
+//entrada de dados
+nome = "Ariane Cristina"
+idade = 30
+peso = 80
+altura = 1.55   
+VIP = true
+
+// saída
+console.log("Ficha do aluno")
+console.log("________________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`VIP: ${VIP}`)
